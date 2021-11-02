d9c43a799451c06fbb073e1134e219f8945c1887
Joe Harrison <josephharrison1@me.com>
2021-11-02 11:15:51 +0000

    updated arduino example and added control model

--- es2c6.ino
+++ es2c6.ino
@@ -5,18 +5,23 @@ Servo servo;  // create servo object to control a servo
 
 int pos = 0;  // variable to store the servo position
 
+int LDRpin = A0;
 void setup() {
     servo.attach(9);  // attaches the servo on pin 9 to the servo object
+    Serial.begin(9600);
 }
 
 void loop() {
+  /*
     for (pos = 0; pos <= 180; pos += 1) {  // goes from 0 degrees to 180 degrees
         // in steps of 1 degree
         servo.write(pos);  // tell servo to go to position in variable 'pos'
-        delay(15);         // waits 15ms for the servo to reach the position
+        delay(5);          // waits 15ms for the servo to reach the position
     }
     for (pos = 180; pos >= 0; pos -= 1) {  // goes from 180 degrees to 0 degrees
         servo.write(pos);                  // tell servo to go to position in variable 'pos'
-        delay(15);                         // waits 15ms for the servo to reach the position
+        delay(5);                          // waits 15ms for the servo to reach the position
     }
+    */
+    Serial.println(analogRead(LDRpin));
 }

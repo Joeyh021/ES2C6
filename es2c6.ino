1b6ae31f22095866438ca3bf86846a9a403831c7
Joe Harrison <josephharrison1@me.com>
2021-11-29 20:51:11 +0000

    new example

--- es2c6.ino
+++ es2c6.ino
@@ -1,27 +1,35 @@
 #include <Servo.h>
 
-Servo servo;  // create servo object to control a servo
-// twelve servo objects can be created on most boards
+Servo s1;
+Servo s2;
 
 int pos = 0;  // variable to store the servo position
 
-int LDRpin = A0;
+int LDRpin[] = {A0,A1,A2,A3};
+int LDRval[4];
 void setup() {
-    servo.attach(9);  // attaches the servo on pin 9 to the servo object
+    s1.attach(9);  // attaches the servo on pin 9 to the servo object
+    s2.attach(11);
     Serial.begin(9600);
 }
 
 void loop() {
-  /*
+    for(int i=0; i <4 ; i++){
+      LDRval[i] = analogRead(LDRpin[i]);
+      Serial.print(LDRval[i]);
+      Serial.print(" ");
+    }
+    Serial.print("\n");
     for (pos = 0; pos <= 180; pos += 1) {  // goes from 0 degrees to 180 degrees
         // in steps of 1 degree
-        servo.write(pos);  // tell servo to go to position in variable 'pos'
-        delay(5);          // waits 15ms for the servo to reach the position
+        s1.write(pos);  
+        s2.write(pos);
+        delay(5);         
     }
     for (pos = 180; pos >= 0; pos -= 1) {  // goes from 180 degrees to 0 degrees
-        servo.write(pos);                  // tell servo to go to position in variable 'pos'
-        delay(5);                          // waits 15ms for the servo to reach the position
+        s1.write(pos);
+        s1.write(pos);                 
+        delay(5);                          
     }
-    */
-    Serial.println(analogRead(LDRpin));
+    
 }

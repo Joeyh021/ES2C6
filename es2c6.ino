14947ad1024d0ba1d4253bf7b80d516c3d8a0552
Joe Harrison <josephharrison1@me.com>
2021-12-01 17:00:54 +0000

    added pid control

--- es2c6.ino
+++ es2c6.ino
@@ -1,13 +1,16 @@
 #include <Servo.h>
+#define ENABLE_ARM true
+#define ENABLE_BASE true
+
+#define DELAY 10
 
 class LDR {
    private:
     int val;
-    uint8_t pin;
+    const uint8_t pin;
 
    public:
-    LDR(int pin) {
-        this->pin = pin;
+    LDR(int pin) : pin{pin} {
     }
 
     int read() {
@@ -15,6 +18,32 @@ class LDR {
     }
 };
 
+class PID {
+   private:
+    const double Kp;
+    const double Ki;
+    const double Kd;
+    const double t_s;
+
+    double I_t_1;
+    double e_t_1;
+
+   public:
+    PID(double Kp, double Ki, double Kd, double t_s) : Kp{Kp}, Ki{Ki}, Kd{Kd}, t_s{t_s} {
+        I_t_1 = 0;
+        e_t_1 = 0;
+    }
+
+    double calculate(double e_t) {
+        double I_t = I_t_1 + e_t * t_s;
+        double D_t = e_t - e_t_1 / t_s;
+
+        // update values for next call
+        e_t_1 = e_t;
+        I_t_1 = I_t;
+        return Kp * e_t + Ki * I_t + Kd * D_t;
+    }
+};
 Servo base;
 Servo arm;
 // A | B
@@ -26,10 +55,8 @@ LDR B(A2);
 LDR C(A1);
 LDR D(A0);
 
-
-#define Kp 0.02
-#define Ki 1
-#define Kd 1
+PID arm_controller(0.02, 0, 0, DELAY);
+PID base_controller(0.02, 0, 0, DELAY);
 
 void LDRdbg() {
     Serial.print("LDR Values: A: ");
@@ -43,52 +70,36 @@ void LDRdbg() {
     Serial.println();
 }
 
-
 void setup() {
     // init serial monitor
     Serial.begin(9600);
     arm.attach(9);
     arm.write(160);
     base.attach(11);
-    base.write(90);
+    base.write(80);
 }
 
 void loop() {
     // calculate the 4 light values
-    int N = A.read() + B.read() / 2;
-    int E = B.read() + D.read() / 2;
-    int S = C.read() + D.read() / 2;
-    int W = A.read() + C.read() / 2;
-    int avg = A.read() + B.read() + C.read() + D.read() / 4;
-    Serial.print(N);
-    Serial.print(" ");
-    Serial.print(E);
-    Serial.print(" ");
-    Serial.print(S);
-    Serial.print(" ");
-    Serial.print(W);
-    Serial.print(" ");
-    Serial.print(arm.read());
-    Serial.print(" ");
-    Serial.print(base.read());
-    Serial.print(" ");
-    Serial.println(" ");
+    int N = (A.read() + B.read()) / 2;
+    int E = (B.read() + D.read()) / 2;
+    int S = (C.read() + D.read()) / 2;
+    int W = (A.read() + C.read()) / 2;
 
-    int baseLight = E - W;
-    int armLight  = S - N;
+    int e_base = E - W;
+    int e_arm = S - N;
 
-    int d_base = (E-W) * Kp;
-    int d_arm = (S-N) * Kp;
+    double d_base = base_controller.calculate(e_base);
+    double d_arm = arm_controller.calculate(e_arm);
 
-    //update values
-    base.write(base.read() + d_base);
-    if(arm.read() + d_arm < 80)
-      arm.write(80);
-    else
-      arm.write(arm.read() + d_arm);
-    
-    delay(10);
-
-  
-
+    // update values
+    if (ENABLE_BASE)
+        base.write(base.read() + d_base);
+    if (ENABLE_ARM) {
+        if (arm.read() + d_arm < 70)
+            arm.write(70);
+        else
+            arm.write(arm.read() + d_arm);
+    }
+    delay(DELAY);
 }
